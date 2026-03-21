e0d71ad24a70402513f8f024ec03dc38912b2d16
AbhiramSakha <143825001+AbhiramSakha@users.noreply.github.com>
2026-03-21 08:36:29 +0530

    Update IterativeBinarySearch.java

--- src/main/java/com/thealgorithms/searches/IterativeBinarySearch.java
+++ src/main/java/com/thealgorithms/searches/IterativeBinarySearch.java
@@ -28,28 +28,26 @@ public final class IterativeBinarySearch implements SearchAlgorithm {
      * @return the index of key in the array or -1 if not found
      */
     @Override
-    public <T extends Comparable<T>> int find(T[] array, T key) {
-        int l;
-        int r;
-        int k;
-        int cmp;
-
-        l = 0;
-        r = array.length - 1;
-
-        while (l <= r) {
-            k = (l + r) >>> 1;
-            cmp = key.compareTo(array[k]);
-
-            if (cmp == 0) {
-                return k;
-            } else if (cmp < 0) {
-                r = --k;
-            } else {
-                l = ++k;
-            }
-        }
-
+public <T extends Comparable<T>> int find(T[] array, T key) {
+    if (array == null || array.length == 0) {
         return -1;
     }
+
+    int l = 0;
+    int r = array.length - 1;
+
+    while (l <= r) {
+        int mid = (l + r) >>> 1;
+        int cmp = key.compareTo(array[mid]);
+
+        if (cmp == 0) {
+            return mid;
+        } else if (cmp < 0) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return -1;
 }
